68762c60c1d75426bf8b4a77050a910c451bfa84
leonidfedorov <leonidf87@gmail.com>
2015-10-18 01:34:17 +0200

    reorganize directory

--- exercise2.hs
+++
@@ -1,4 +0,0 @@
-last_alt1 xs  = drop (length xs - 1) xs --works on empty lists unlike original last
-last_alt2 xs = head (drop (length xs - 1) xs) --doesn't work on empty lists just like original last
-last_alt3 xs = xs !! (length xs - 1) --doesn't work on empty lists though just like original last, but the error is different
-last_alt4 xs = head (reverse xs) --doesn't work on empty lists just like original last
--- test.hs
+++
@@ -1,9 +0,0 @@
-double x = x + x
-quadruple x = double (double x)
-factorial n = product [1..n]
-average ns = sum ns `div` length ns
-a = b + c
-	where
-		b = 1
-		c = 2
-d = a * 2
